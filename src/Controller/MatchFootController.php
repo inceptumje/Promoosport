2eefac56000e7c14b469f1f12b1b9ead2f180cd3
AbdAzizFitouri69 <mohamedabdaziz.fitouri@esprit.tn>
2022-03-19 12:04:52 +0100

    Clients can now see added matches, howerver encoutred problems when filtering by leagues | 202203191205

--- src/Controller/MatchFootController.php
+++ src/Controller/MatchFootController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Repository\MatchFootRepository;
+use App\Repository\LeagueRepository;
 
 use App\Entity\MatchFoot;
 use App\Form\MatchFootType;
@@ -36,4 +37,53 @@ class MatchFootController extends AbstractController
             'matchees' => $matches
         ]);
     }
+
+    /**
+     * @Route("/match", name="front_match")
+     */
+    public function accessFront(Request $request, MatchFootRepository $mfrp, LeagueRepository $lrp): Response
+    {
+        $matches = $mfrp->findAll();
+        $leagues = $lrp->findAll();
+
+        return $this->render('MatchFoot/Front-Office/match.front.html.twig', [
+            'controller_name' => 'MatchFootController',
+            'matchees' => $matches,
+            'leagues' => $leagues
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/match/{leagueId}", name="admin_match_by_league")
+     */
+    public function getMatchesByLeague(MatchFootRepository $mfrp, LeagueRepository $lrp, $leagueId): Response
+    {
+
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $dbname = "promosport2";
+
+        // $conn = new \mysqli($servername, $username, $password, $dbname);
+
+        // $sql = "SELECT  FROM match_foot WHERE league_id = ".$leagueId;
+        // $matches = $conn->query($sql);
+
+        // $conn->close();
+
+
+        $matches = $lrp->selectMatchesOfLeague($leagueId);
+        $leagues = $lrp->findAll();
+
+        return $this->render('MatchFoot/Front-Office/match.front.html.twig', [
+            'controller_name' => 'MatchFootController',
+            'matchees' => $matches,
+            'leagues' => $leagues
+        ]);
+
+    }
+
 }
