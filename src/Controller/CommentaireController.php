6836a1101f891049d4c25f450595c5d4d029d5f7
MO7S3N <rider1@outlook.fr>
2022-03-22 14:43:28 +0100

    user + commentaire ( back-office)

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -2,11 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentaireController extends AbstractController
 {
+    /**
+     * @Route("/Dashboard/commentaires", name="Dashboard_commentaire")
+     */
+    public function index_back(CommentaireRepository $repository ): Response
+    {
+        $commentaires = $repository->findAll();
+        return $this->render('commentaire/indexBack.html.twig', [
+            "commentaires" => $commentaires
+        ]);
+    }
 
+    /**
+     * @Route("/Dashboard/commentaire/supprimer/{id}", name="supprimer_commentaire")
+     */
+    public function delete(Commentaire $commentaire)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($commentaire);
+        $em->flush();
+        $this->addFlash('success',"L'action a ete effectué");
+        return $this->redirectToRoute('Dashboard_commentaire');
+    }
 }
